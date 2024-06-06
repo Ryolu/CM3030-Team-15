c29f6eb38ed1dfee4aa87b88fdcfe2241e3aa2bb
Jasper <rendescar63@gmail.com>
2024-06-06 14:22:47 +0800

    first commit

---
+++ CM3030/Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public Vector3 target;
+    private float moveSpeed = .25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new Vector3(0, 0, -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, target) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed);
+        }
+    }
+}
