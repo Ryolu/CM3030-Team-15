6fbc7ee82bfcdeea5e447bbe06fa382e063b7bfa
Jasper <rendescar63@gmail.com>
2024-09-03 23:05:55 +0800

    new sprite testing + code fix

--- CM3030/Assets/Scripts/level/LevelManager.cs
+++ CM3030/Assets/Scripts/level/LevelManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Overall level manager.
+/// </summary>
 public class LevelManager : MonoBehaviour
 {
     public MapManager MM;
@@ -11,8 +14,8 @@ public class LevelManager : MonoBehaviour
     public Transform roomParent;
     public int currentLevel = 1;
 
-    private List<GameObject> rooms = new List<GameObject>();
-    private int currentRoom = 0;
+    List<GameObject> rooms = new List<GameObject>();
+    int currentRoom = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,7 @@ public class LevelManager : MonoBehaviour
             MM.InitRooms(currentLevel);
         }
 
+        // room changing test code
         if(Input.GetKeyDown(KeyCode.KeypadPlus))
         {
             ChangeRoom(1);
@@ -40,14 +44,20 @@ public class LevelManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Generate the level based on the provided locations.
+    /// </summary>
+    /// <param name="locations">Pre-generated locations of rooms.</param>
     void GenerateLevel(List<Vector3> locations)
     {
         foreach(Vector3 location in locations)
-        {
-            rooms.Add(Instantiate(roomPrefab, Vector3.Scale(location, roomDimensions), Quaternion.identity, roomParent));
-        }
+            rooms.Add(Instantiate(roomPrefab, Vector3.Scale(location, roomDimensions * 1.1f), Quaternion.identity, roomParent));
     }
 
+    /// <summary>
+    /// Room change sequence.
+    /// </summary>
+    /// <param name="value">Room to change to.</param>
     void ChangeRoom(int value)
     {
         currentRoom += value;
--- CM3030/Assets/Scripts/level/MapManager.cs
+++ CM3030/Assets/Scripts/level/MapManager.cs
@@ -2,113 +2,152 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 
+/// </summary>
 public class MapManager : MonoBehaviour
 {
 	public GameObject room;
 	public GameObject door;
     public Camera miniCam;
     public Transform minimap;
-
-    private int numRooms = 0;
     public int roomLimit;
-    private Vector3[] directions = { new Vector3(1, 0, 0), new Vector3(0, -1, 0), new Vector3(0, 1, 0), new Vector3(0, -1, 0) };
-    private List<Vector3> locations = new List<Vector3>();
-    private List<GameObject> rooms = new List<GameObject>();
-    private List<GameObject> doors = new List<GameObject>();
-    private GameObject playerIndicator;
-	
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
+    int numRooms = 0;
+    Vector3[] directions = { new Vector3(1, 0, 0), new Vector3(0, -1, 0), new Vector3(0, 1, 0), new Vector3(0, -1, 0) };
+    List<Vector3> locations = new List<Vector3>();
+    List<GameObject> rooms = new List<GameObject>();
+    List<GameObject> doors = new List<GameObject>();
+    GameObject playerIndicator;
+	
+    /// <summary>
+    /// Start creating the rooms based on the current level.
+    /// </summary>
+    /// <param name="currentLevel">Current level.</param>
+    /// <returns></returns>
     public List<Vector3> InitRooms(int currentLevel)
     {
+        // reset all rooms first
         ResetRooms();
+
+        // update the room limit
         roomLimit = currentLevel * 2 + 1;
 
+        // generate the rooms until there are no exceptions thrown
         while (numRooms == 0)
-        {
-            GenerateRooms(currentLevel);
-        }
+            GenerateRooms();
 
+        // return the center point of each room
         return locations;
     }
 
-    private void Update()
+    /// <summary>
+    /// Spawn the rooms based on current level.
+    /// </summary>
+    /// <param name="currentLevel">Current level.</param>
+    void GenerateRooms()
     {
+        SpawnRooms();
 
-    }
-
-    void GenerateRooms(int currentLevel)
-    {
-        SpawnRooms(currentLevel);
-
+        // reset all rooms if the number of rooms is too low
         if (numRooms != roomLimit)
             ResetRooms();
     }
 
-    void SpawnRooms(int currentLevel)
+    /// <summary>
+    /// Spawn the rooms.
+    /// </summary>
+    void SpawnRooms()
     {
-        Vector3 start = Vector3.zero;
-        playerIndicator = Instantiate(door, start, Quaternion.identity, minimap);
+        // make the player indicator start from the center
+        playerIndicator = Instantiate(door, Vector3.zero, Quaternion.identity, minimap);
         playerIndicator.transform.localScale *= 2.5f;
         playerIndicator.GetComponent<SpriteRenderer>().color = Color.green;
-        //SpawnRandom(start, Vector3.zero);
-        SpawnStraight(start);
 
+        // actually spawn the rooms now
+        //SpawnRandom(Vector3.zero, Vector3.zero);
+        SpawnStraight();
+
+        // starting rooms is blue
         rooms[0].GetComponent<SpriteRenderer>().color = Color.blue;
+        // bossroom is reed
         rooms[roomLimit - 1].GetComponent<SpriteRenderer>().color = Color.red;
 
-        Vector3 center = Vector3.zero;
-        center.z = -10;
-        foreach (Vector3 pos in locations)
-            center += pos;
-
-        center /= locations.Count;
-        miniCam.transform.position = center;
-        if (currentLevel > 5)
-            miniCam.GetComponent<Camera>().orthographicSize = currentLevel + 0.5f;
+        // center the minimap camera onto the room the player starts from
+        Vector3 start = Vector3.zero;
+        start.z = -10f;
+        miniCam.transform.position = start;
     }
 
-    void SpawnStraight(Vector3 start)
+    /// <summary>
+    /// Spawn rooms in a straight line
+    /// </summary>
+    void SpawnStraight()
     {
-        Vector3 roomPos;
         for (int i = 0; i < roomLimit; ++i)
         {
-            roomPos = start + new Vector3(0, i, 0);
+            // determine the room position
+            Vector3 roomPos = new Vector3(0, i, 0);
+            // create rooms and add to list
             rooms.Add(Instantiate(room, roomPos, Quaternion.identity, minimap));
+            // add room center
             locations.Add(roomPos);
+            // add door if not first room
             if (i != 0)
                 doors.Add(Instantiate(door, roomPos - Vector3.up / 2, Quaternion.identity, minimap));
             numRooms++;
         }
     }
 
+    /// <summary>
+    /// Spawn rooms in a random manner.
+    /// </summary>
+    /// <param name="pos">Position of new room.</param>
+    /// <param name="doorPos">Position of door to previous room.</param>
     void SpawnRandom(Vector3 pos, Vector3 doorPos)
     {
+        // if room loops back to origin
         if (numRooms > 0 && pos.Equals(Vector3.zero))
             return;
+        // if room already exists in new location or limit has been reached
         if (locations.Contains(pos) || numRooms == roomLimit)
             return;
 
+        // create room and add location to list
         rooms.Add(Instantiate(room, pos, Quaternion.identity, minimap));
         locations.Add(pos);
         numRooms++;
 
+        // room not at origin, add door
         if (pos != Vector3.zero)
             doors.Add(Instantiate(door, pos - doorPos, Quaternion.identity, minimap));
 
+        // shuffle directions then spawn room there based on chance
+        Shuffle(directions);
         for (int i = 0; i < 4; i++)
-        {
-            bool generate = Random.Range(0f, 1f) < 0.3 ? true : false;
-            if (generate)
-            {
+            if (Random.Range(0f, 1f) < 0.25f)
                 SpawnRandom(pos + directions[i], directions[i] / 2);
-            }
+    }
+
+    /// <summary>
+    /// Fisher-Yates shuffle on generic list
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    /// <param name="list">List</param>
+    void Shuffle<T>(IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
         }
     }
 
+    // resets anything map related
     void ResetRooms()
     {
         numRooms = 0;
@@ -123,8 +162,13 @@ public class MapManager : MonoBehaviour
         Destroy(playerIndicator);
     }
 
+    /// <summary>
+    /// Move the minimap camera and player indicator to the room based on argument value.
+    /// </summary>
+    /// <param name="value">Room to move to.</param>
     public void ChangeRoom(int value)
     {
         playerIndicator.transform.position = locations[value];
+        miniCam.transform.position = locations[value] - new Vector3(0, 0, 10);
     }
 }
